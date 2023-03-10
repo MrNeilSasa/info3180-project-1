11ce03627a3fb14567af161d54a095bccdfb13cf
Antonio Neil <bossneil20@gmail.com>
2023-03-10 13:39:31 -0500

    First commit New Property page can be displayed

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ load_dotenv()  # load environment variables from .env if it exists.
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
-    #SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'aef10f304d32e59c10840bd587dd42909ed1264ea03e5b0a8e41a74c668f95cb')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
---
+++ app/property.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField
+from wtforms.validators import InputRequired, Email
+from flask import Markup
+
+class NewPropertyForm(FlaskForm):
+    title = StringField(Markup('Property Title'), validators=[InputRequired()])
+    description = TextAreaField(Markup('Description'), validators=[InputRequired()])
+    rooms = StringField(Markup('No. of Rooms'), validators=[InputRequired()])
+    bathrooms = StringField(Markup('No. of Bathrooms'), validators=[InputRequired()])
+    price = StringField(Markup('Price'), validators=[InputRequired()])
+    
+    location = StringField(Markup('Location'), validators=[InputRequired()])
+    file = FileField('Upload Image Here: ', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Only JPEG and PNG images are allowed!')])
+
